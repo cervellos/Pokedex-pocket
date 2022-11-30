f6ada8c633a8541c668368d1bf63409518e6875b
Freddy Guerra <arcobaleno262@gmail.com>
2022-11-30 20:29:26 -0300

    archivo prettier

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+//{
+//() => {
 console.log(document.querySelector("title").innerText);
+
+url = "https://pokeapi.co/api/v2/pokemon/133/";
+
+document.addEventListener("DOMContentLoaded", () => {
+  fechtData;
+});
+const fechtData = fetch(url)
+  // Exito
+  .then((response) => response.json()) // convertir a json
+  .then((json) => console.log(json)) //imprimir los datos en la consola
+  .catch((err) => console.log("Solicitud fallida", err)); // Capturar errores
+
+const template = document.getElementById("template").content;
+// };
+//}
