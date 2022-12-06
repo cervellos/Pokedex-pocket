f1ae3f54d897cad60bdf4b529b493df59714148e
Freddy Guerra <arcobaleno262@gmail.com>
2022-12-06 00:46:09 -0300

    handlebars implementado

--- js/main.js
+++ js/main.js
@@ -18,22 +18,25 @@ const fechtData = async (id) => {
   try {
     console.log(id)
 
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    const data = await res.json()
-
-    console.log(data)
-
+    const res1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const data1 = await res1.json()
+    const res2 = await fetch(data1.moves[0].move.url)
+    const data2 = await res2.json()
+    console.log(data2)
     const pokemon = {
-      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`,
-      imgJuego: data.sprites.front_default,
-      imgCvg: data.sprites.other.dream_world.front_default,
-      nombre: data.name,
-      experiencia: data.base_experience,
-      hp: data.stats[0].base_stat,
-      ataque: data.stats[1].base_stat,
-      defensa: data.stats[2].base_stat,
-      especial: data.stats[3].base_stat,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data1.id}.png`,
+      imgJuego: data1.sprites.front_default,
+      imgCvg: data1.sprites.other.dream_world.front_default,
+      nombre: data1.name,
+      experiencia: data1.base_experience,
+      hp: data1.stats[0].base_stat,
+      ataque: data1.stats[1].base_stat,
+      defensa: data1.stats[2].base_stat,
+      especial: data1.stats[3].base_stat,
+      moveName: data1.moves[0].move.name,
+      moveDes: data2.effect_entries[0].effect,
     }
+    console.log(pokemon)
 
     pintarCard(pokemon)
   } catch (error) {
@@ -41,26 +44,15 @@ const fechtData = async (id) => {
   }
 }
 
-const pintarCard = (pokemon) => {
-  const flex = document.querySelector(".flex")
-  const template = document.getElementById("card").content
-  const clone = template.cloneNode(true)
-  const fragment = document.createDocumentFragment()
-
-  clone.querySelector(".card-body-img").setAttribute("src", pokemon.imgCvg)
-  // clone.querySelector('.card-body-img').setAttribute('src', pokemon.imgJuego)
-  clone.querySelector(
-    ".card-body-title"
-  ).innerHTML = `${pokemon.nombre} <span>${pokemon.hp}hp</span>`
-  clone.querySelector(".card-body-text").textContent =
-    pokemon.experiencia + " exp"
-  clone.querySelectorAll(".card-footer-social h3")[0].textContent =
-    pokemon.ataque + "K"
-  clone.querySelectorAll(".card-footer-social h3")[1].textContent =
-    pokemon.especial + "K"
-  clone.querySelectorAll(".card-footer-social h3")[2].textContent =
-    pokemon.defensa + "K"
-
-  fragment.appendChild(clone)
-  flex.appendChild(fragment)
+const pintarCard = async (pokemon) => {
+  try {
+    const res = await fetch("templates/card.hbs")
+    const templateHbs = await res.text()
+    const template = Handlebars.compile(templateHbs)
+    const html = template({ pokemon })
+    console.log(html)
+    document.getElementsByClassName("cards-container")[0].innerHTML = html
+  } catch (err) {
+    console.error(`este error${err}`)
+  }
 }
